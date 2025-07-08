46122c8efc787d6c59efbd023cd039c8c57f2b83
mariana5757 <mariana.ortiz@pragma.com.co>
2025-07-08 11:06:37 -0500

    style: estilo de botón desactivado

--- src/app/components/pages/index/index.component.ts
+++ src/app/components/pages/index/index.component.ts
@@ -18,30 +18,35 @@ export class IndexComponent {
       icon: 'seed.png',
       description:
         'Primeros pasos. Aprendes los fundamentos y te introduces al ecosistema frontend.',
+      button: 'active'
     },
     {
       id: 1,
       name: 'Junior',
       icon: 'junior.png',
       description: 'Ya puedes construir. Escribes código funcional y entiendes cómo trabajar en equipo.',
+      button: 'deactive'
     },
     {
       id: 2,
       name: 'Advanced',
       icon: 'advanced.png',
       description: 'Dominas herramientas y patrones. Optimización, buenas prácticas y autonomía.',
+      button: 'deactive'
     },
     {
       id: 3,
       name: 'Senior',
       icon: 'senior.png',
       description: 'Diseñas soluciones, lideras decisiones técnicas y apoyas al equipo',
+      button: 'deactive'
     },
     {
       id: 4,
       name: 'Master',
       icon: 'master.png',
       description: 'Influencia estratégica. Innovas, inspiras y haces crecer a otros.',
+      button: 'deactive'
     },
   ];
 
